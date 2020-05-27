4997b5e04c98ea7f5a32b7ba764b8b572903ee2e
anwesh0304 <anwesh0304@gmail.com>
2020-05-27 22:03:31 +0530

    Added deletion of object files to install.bat

--- install.bat
+++ install.bat
@@ -9,5 +9,13 @@ gcc -w -g -c ast.c
 gcc -w -g -c typeChecker.c
 gcc -w -g -c lexer.c
 gcc -w -g -c "%grmPath%"
-gcc -w -g -c symbolTable.c
 gcc -w -g -o .compiler erplag.o codegen.o ast.o lexer.o parser.o symbolTable.o typeChecker.o error.o
+
+del erplag.o
+del error.c
+del codegen.o
+del symbolTable.o
+del ast.o
+del typeChecker.o
+del lexer.o
+del parser.c
